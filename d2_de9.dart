939f4cbde31b458a1703e6b60748709a90705a29
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-11 16:16:50 -0300

    DE9 Concluida

--- d2_de9.dart
+++ d2_de9.dart
@@ -1,1 +1,71 @@
-void main() {}
+void main() {
+  final controleDePessoas = ControleDePessoas();
+
+  controleDePessoas
+    ..cadastrarPessoa(Pessoa('Jose', Mes.abril))
+    ..cadastrarPessoa(Pessoa('Arthur', Mes.agosto))
+    ..cadastrarPessoa(Pessoa('Joao', Mes.abril))
+    ..cadastrarPessoa(Pessoa('Jesse', Mes.dezembro))
+    ..cadastrarPessoa(Pessoa('Roberta', Mes.fevereiro))
+    ..cadastrarPessoa(Pessoa('Carla', Mes.fevereiro))
+    ..cadastrarPessoa(Pessoa('Thania', Mes.agosto))
+    ..cadastrarPessoa(Pessoa('Rafaela', Mes.marco))
+    ..cadastrarPessoa(Pessoa('Yuri', Mes.junho))
+    ..cadastrarPessoa(Pessoa('Jonas', Mes.setembro))
+    ..cadastrarPessoa(Pessoa('Elias', Mes.outubro))
+    ..cadastrarPessoa(Pessoa('Abel', Mes.maio))
+    ..cadastrarPessoa(Pessoa('Danilo', Mes.abril))
+    ..cadastrarPessoa(Pessoa('Jonathan', Mes.abril))
+    ..cadastrarPessoa(Pessoa('Joseph', Mes.setembro))
+    ..cadastrarPessoa(Pessoa('Abdul', Mes.janeiro))
+    ..cadastrarPessoa(Pessoa('Jean', Mes.abril));
+
+  for (final mes in controleDePessoas.mesesComPessoas) {
+    print('\n${mes.name}');
+
+    for (final pessoa in controleDePessoas.pessoasPorMes(mes)) {
+      print(' > ${pessoa.nome}');
+    }
+  }
+}
+
+enum Mes {
+  janeiro,
+  fevereiro,
+  marco,
+  abril,
+  maio,
+  junho,
+  julho,
+  agosto,
+  setembro,
+  outubro,
+  novembro,
+  dezembro
+}
+
+class Pessoa {
+  Pessoa(this.nome, this.mesDeNascimento);
+
+  final String nome;
+  final Mes mesDeNascimento;
+}
+
+class ControleDePessoas {
+  final _pessoasPorMes = <Mes, List<Pessoa>>{};
+
+  void cadastrarPessoa(Pessoa pessoa) {
+    final mes = pessoa.mesDeNascimento;
+    _pessoasPorMes[mes] ??= [];
+    _pessoasPorMes[mes]!.add(pessoa);
+  }
+
+  List<Mes> get mesesComPessoas {
+    return _pessoasPorMes.keys.toList()
+      ..sort((a, b) => a.index.compareTo(b.index));
+  }
+
+  List<Pessoa> pessoasPorMes(Mes mes) {
+    return _pessoasPorMes[mes] ?? [];
+  }
+}
